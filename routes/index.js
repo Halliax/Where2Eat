1816aa2ef23374cab80345aaa76e4e1acbd902bf
Halliax <tmsheneman@gmail.com>
2017-03-06 21:13:01 -0500

    removed useless side effects from index.js, last push to master I swear

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,8 @@ Robot.find({}, function(err, robots){
   console.log(robots);
 });
 
-snipBot = new Robot({name:'SnipBot',abilities:["snipping","clacking"],isEvil:'false'});
-snipBot.save(function(err) {
-  if(err) {
-    console.log("save error:", err);
-  }
-});
-
-var home = function(req, res){
+routes.home = function(req, res){
   res.render("home", {"robots": [snipBot]});
 };
 
-module.exports.home = home;
+module.exports = routes;
