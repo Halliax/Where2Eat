4e0fcd907bfd1e54d113b2b06e9938f18a4ea094
Halliax <tmsheneman@gmail.com>
2017-03-20 20:16:25 -0400

    a gently rotating burger logo now graces the homepage. Also the favicon, though that doesn't rotate

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
-import logo from './logo.svg';
+import logo from './burger.svg';
 import './App.css';
 import HomepageContainer from './components/HomepageContainer';
 
