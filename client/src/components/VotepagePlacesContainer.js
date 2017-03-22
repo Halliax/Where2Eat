d4ad1f2c0a344c1d6282e88012e4bd99f8d370c5
Halliax <tmsheneman@gmail.com>
2017-03-22 00:43:40 -0400

    added styling for vote page (incomplete)

--- client/src/components/VotepagePlacesContainer.js
+++ client/src/components/VotepagePlacesContainer.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 
 
 class VotepagePlacesContainer extends Component {
-  
+
   state = {
 
   }
@@ -22,11 +22,29 @@ class VotepagePlacesContainer extends Component {
     }
     else {
       var listItems = places.map(function(item,i){
-        return <li>{item}</li>
+        // var photo = item.icon;
+        // if (item.hasOwnProperty("photos")) {
+        //   photo = item.photos[0].getUrl({'maxWidth': 45, 'maxHeight': 45});
+        // }
+        return (
+          <li key={item.place_id} className="list-group-item">
+            <div className="media">
+              <div className="media-left">
+                <a href="#">
+                  <img className="media-object" src="{photo}" alt="place"/>
+                </a>
+              </div>
+              <div className="media-body">
+                <h4 className="media-heading">{item}</h4>
+                  Rating: {item.rating}   Price level: {item.price_level}
+              </div>
+            </div>
+          </li>
+        )
       });
       return (
           <div>
-            <ul>
+            <ul className="list-group">
               {listItems}
             </ul>
           </div>
